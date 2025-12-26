6dbd3f2021e778a38df9f879a4c8183c224abc56
Efdal Gözübüyük <efdalgozubuyuk@gmail.com>
2025-12-26 16:06:11 +0300

    Add files via upload

---
+++ deneme.c
@@ -0,0 +1,34 @@
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    if (msg == WM_DESTROY) {
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+int main() {
+    WNDCLASS wc = {0};
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = GetModuleHandle(NULL);
+    wc.lpszClassName = "MyWindow";
+
+    RegisterClass(&wc);
+
+    CreateWindow(
+        "MyWindow",
+        "C GUI Window",
+        WS_OVERLAPPEDWINDOW,
+        100, 100, 400, 300,
+        NULL, NULL, wc.hInstance, NULL
+    );
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return 0;
+}
+
