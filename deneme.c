4d5119ee48af1cec000913d8611bfa36ba9ea412
Efdal Gözübüyük <efdalgozubuyuk@gmail.com>
2025-12-26 20:21:50 +0300

    Fix

--- deneme.c
+++ deneme.c
@@ -1,34 +1,7 @@
 #include <windows.h>
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    if (msg == WM_DESTROY) {
-        PostQuitMessage(0);
-        return 0;
-    }
-    return DefWindowProc(hwnd, msg, wParam, lParam);
-}
-
 int main() {
-    WNDCLASS wc = {0};
-    wc.lpfnWndProc = WndProc;
-    wc.hInstance = GetModuleHandle(NULL);
-    wc.lpszClassName = "MyWindow";
-
-    RegisterClass(&wc);
-
-    CreateWindow(
-        "MyWindow",
-        "C GUI Window",
-        WS_OVERLAPPEDWINDOW,
-        100, 100, 400, 300,
-        NULL, NULL, wc.hInstance, NULL
-    );
-
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
+    // Basit bir uyarı kutusu
+    MessageBox(NULL, "Merhaba! Bu C ile yazilmis bir mesaj kutusudur.", "Baslik", MB_OK | MB_ICONINFORMATION);
     return 0;
 }
-
